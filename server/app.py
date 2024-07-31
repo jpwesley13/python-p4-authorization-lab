b6b87b7e08773d76238e90d264a87ab229326490
Jared Patterson <jpwesley34@gmail.com>
2024-07-31 12:38:49 -0700

    Pass tests.

--- server/app.py
+++ server/app.py
@@ -18,6 +18,13 @@ db.init_app(app)
 
 api = Api(app)
 
+@app.before_request
+def check_if_signed_in():
+    ok_endpoints = ['clear', 'article_list', 'show_article', 'login', 'logout', 'check_session']
+
+    if not session.get('user_id') and request.endpoint not in ok_endpoints:
+        return {'error': 'Unauthorized'}, 401
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -87,12 +94,14 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        articles = Article.query.filter(Article.is_member_only == True).all()
+        return [article.to_dict() for article in articles], 200
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        article = Article.query.filter_by(id=id).first()
+        return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
